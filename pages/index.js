742c4b8bcbb3f6d9d28df137b3e0766fac39406b
VernaMai <vmai1@my.bcit.ca>
2023-03-22 08:39:06 -0700

    Added alert message, 5 Tests and other css updates

--- pages/index.js
+++ pages/index.js
@@ -68,32 +68,38 @@ useEffect(() => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main className={styles.main} id="main">
      
       <Currency test={test} setTest={setTest} con1={con1} con2={con2} setCon1={setCon1} setCon2={setCon2}/>
 {/* 
       <div>{con1}</div>
       <div>{con2}</div> */}
 
-      {(() => {
-        if (con1 && con2) {
-          return (
-            <>
-              {record.filter (r => r.currency_code === con1 || r.currency_code === con2).map((r,i) => (
-                <div key={i} className={`${styles.con_cnt} ${r.currency_code === con1 ? styles.con1 : styles.con2}`}>
-                  {r.currency_code === con1 && <div className={styles.con_h2}>{con1}</div>}
-                  {r.currency_code === con2 && <div className={styles.con_h2}>{con2}</div>}
-                  <div > Local Price: {r.local_price}</div>
-                  <div> Dollar Exchange: {r.dollar_ex}</div>
-                  <div> Dollar Price: {r.dollar_price}</div>
-                </div>
-              ))}
-            </>
-          )
-        } else {
-          return null;
-        }
-      })()}
+     
+             {record
+  .filter(r => r.currency_code === con1 || r.currency_code === con2)
+  .sort((a, b) => {
+    if (a.currency_code === con1) {
+      return -1;
+    } else if (b.currency_code === con1) {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+  .map((r,i) => (
+    <div key={i} className={`${styles.con_cnt} ${r.currency_code === con1 ? styles.con1 : styles.con2}`}>
+      {r.currency_code === con1 && <div className={styles.con_h2}>{con1}</div>}
+      {r.currency_code === con2 && <div className={styles.con_h2}>{con2}</div>}
+  
+      <div > Local Price: &nbsp; <span style={{color: '#fcc219',fontWeight:'bold'}}>{r.local_price}</span></div>
+      <div> Dollar Exchange:&nbsp; <span style={{color: '#fcc219',fontWeight:'bold'}}> {r.dollar_ex} </span></div>
+      <div> Dollar Price: &nbsp; <span style={{color: '#fcc219',fontWeight:'bold'}}> {r.dollar_price} </span></div>
+      <div className={styles.symbol}  style={r.currency_code === con2 ? {right: '210px', position: 'absolute',fontSize:'80px'} : {}}>{r.dollar_symbol}</div>
+    </div>
+))}
+             
+     
 
     
 
